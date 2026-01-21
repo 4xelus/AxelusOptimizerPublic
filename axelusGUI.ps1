3a3a16dd3e603cbee1cb16973fd5c49bd2d96a26
4xelus <mostekmateusz@gmail.com>
2026-01-21 11:01:51 +0100

    axelusGUI.ps1

--- axelusGUI.ps1
+++ axelusGUI.ps1
@@ -1,4 +1,5 @@
-# Wczytanie funkcji i XAML relatywnie
+Add-Type -AssemblyName PresentationFramework
+
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 . "$scriptDir\AxelusOptimizer.ps1"
 
@@ -6,7 +7,6 @@ $XAMLPath = "$scriptDir\axelusGUI.xaml"
 $reader = (New-Object System.Xml.XmlNodeReader (Get-Content $XAMLPath -Raw))
 $Form = [Windows.Markup.XamlReader]::Load($reader)
 
-# Przyciski
 $logBox = $Form.FindName("logBox")
 $btnPowerPlan = $Form.FindName("btnPowerPlan")
 $btnGameDVR = $Form.FindName("btnGameDVR")
@@ -16,10 +16,11 @@ $btnServices = $Form.FindName("btnServices")
 $btnGPU = $Form.FindName("btnGPU")
 $btnRestart = $Form.FindName("btnRestart")
 
-# Funkcja logowania
-function Log($Action) { $logBox.AppendText("$Action`r`n") }
+function Log($Action) {
+    $logBox.AppendText("$Action`r`n")
+    $logBox.ScrollToEnd()
+}
 
-# Kliknięcia
 $btnPowerPlan.Add_Click({ Log (Set-PowerPlan) })
 $btnGameDVR.Add_Click({ Log (Disable-GameDVR) })
 $btnInputLatency.Add_Click({ Log (Input-LatencyTweaks) })
@@ -28,6 +29,5 @@ $btnServices.Add_Click({ Log (Services-Debloat) })
 $btnGPU.Add_Click({ Log (GPU-Tweaks) })
 $btnRestart.Add_Click({ Restart-Computer })
 
-# Pokaż GUI
 $Form.ShowDialog() | Out-Null
 
